d880fceca0268fa604f30b10f933f16a8d4b407f
yunisrasulzade <yunisrasulzada99@gmail.com>
2020-07-08 23:16:12 +0400

    Initial commit

---
+++ app/src/main/java/com/example/test5/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.test5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
